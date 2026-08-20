d742f4aa7198caf97d9971db10422e908135dacc
dehghanzademahdi-netizen <dehghanzade.mahdi@gmail.com>
2026-08-20 09:44:41 +0330

    Enable Compose build and bump ChemTrade APK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,17 @@ plugins {
 android {
     namespace = "com.dehghanzadeh.chemtrade"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.dehghanzadeh.chemtrade"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
+    }
+
+    buildFeatures {
+        compose = true
     }
 }
 
