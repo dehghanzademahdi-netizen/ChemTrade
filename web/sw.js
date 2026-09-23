13352a6f8439456fec4b3e6b9ca1089fc45158ec
dehghanzademahdi-netizen <dehghanzade.mahdi@gmail.com>
2026-09-23 10:46:38 +0330

    Refresh web cache and include iPhone icon

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,5 @@
-const CACHE='chemlink-web-v6';
-const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./icons/chemlink-192.svg','./icons/chemlink-512.svg','./icons/chemlink-180.svg'];
+const CACHE='chemlink-web-v7';
+const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./icons/chemlink-192.svg','./icons/chemlink-512.svg','./icons/chemlink-180.svg','./icons/chemlink-180.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
